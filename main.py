4a22a6d9b88a92a4e0763f38a15da1ea3d787578
imran-samed <imransamed7@gmail.com>
2021-12-09 01:15:42 +0530

    build simple turtle race just run and enjoy

--- main.py
+++ main.py
@@ -1,9 +1,37 @@
-import prettytable
+import turtle
+from turtle import Turtle, Screen
+import random
 
+is_race_on = True
+screen = Screen()
+screen.setup(width=500, height=500)
+user_bet = screen.textinput("Let's bet", "Which color turtle will win ?")
 
-table = prettytable.PrettyTable()
-table.add_column("Sr No.", ["1", "2", "3"])
-table.add_column("Name", ["Imran", "Anwar", "Iqbal"])
+colors = ["red", "green", "yellow", "blue", "purple", "green"]
+all_turtle = []
 
+init_point = - 175
+for c in range(0, len(colors)):
+    t = Turtle(shape="turtle")
+    t.penup()
+    t.color(colors[c])
+    init_point = init_point + 50
+    t.goto(-230, init_point)
+    all_turtle.append(t)
 
-print(table)
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for t in all_turtle:
+        if t.xcor() > 230:
+            is_race_on = False
+            if t.pencolor() == user_bet:
+                print(f"You've won. The wining turtle color is {t.pencolor()}")
+            else:
+                print(f"You've lost. The wining turtle color is {t.pencolor()}")
+
+        move_by = random.randint(0, 10)
+        t.forward(move_by)
+
+screen.exitonclick()
